41f87c9d10f1a0c1b055128b3a20270696415452
Ben Muthalaly <benmuthalaly@gmail.com>
2023-04-06 14:34:01 -0500

    Get sounds synced with counting.
    App now plays a chord in sync with an incrementing binary number.
    Also added preliminary styling.

--- js/n-ary-piano.js
+++ js/n-ary-piano.js
@@ -1,65 +1,35 @@
-// import * as Tone from "tone";
-
-// "use strict";
+"use strict";
 // there can be multiple "keyboards"
 // each keyboard has data: a radix, current count
 
-// counts from 0 til base^9
-// console.log(1);
-// document.querySelector("button").addEventListener("click", async () => {
-//   await Tone.start();
-// });
+var number = document.getElementById("number");
 
-// window.onclick = new AudioContext();
-// let synth = new Tone.PolySynth().toDestination();
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
 
-addEventListener("click", (event) => {
-  const synth = new Tone.PolySynth().toDestination();
-  // set the attributes across all the voices using 'set'
-  synth.set({ detune: -1200 });
-  // play a chord
-  synth.triggerAttackRelease(["C4", "E4", "A4"], 1);
+startButton.addEventListener("click", () => {
+  let synth = new Tone.PolySynth(Tone.Synth, {
+    oscillator: {
+      partials: [0, 2, 3, 4],
+    },
+  }).toDestination();
+
+  Tone.Transport.bpm.value = 50;
+  //play a note every eighth note starting from the first measure
+  Tone.Transport.scheduleRepeat(
+    function (time) {
+      // play a chord
+      synth.triggerAttackRelease(["C4", "E4", "A4"], 0.4, time);
+      number.dataset.val++;
+
+      number.innerHTML = (number.dataset.val >>> 0).toString(2);
+    },
+    "8n",
+    "1m",
+  );
+  Tone.Transport.start();
 });
 
-// console.log(synth.get);
-
-// piano({
-//   parent: document.querySelector("#number"),
-//   polyphonic: true,
-//   noteon: (note) => synth.triggerAttack(note.name),
-//   noteoff: (note) => synth.triggerRelease(note.name),
-// });
-
-// ui({
-//   tone: synth,
-//   parent: document.querySelector("#number"),
-// });
-// console.log(2);
-// function count() {
-//   let numbers = document.getElementsByClassName("number");
-//   let base = 5;
-//   let limit = base ** 9;
-//   let str = "0";
-
-//   const synth = new Tone.PolySynth(8, Tone.Synth, {
-//     oscillator: {
-//       partials: [0, 2, 3, 4],
-//     },
-//   });
-//   // console.log(synth.getOwnPropertyNames());
-//   synth.toDestination();
-//   Tone.Transport.bpm.value = 50;
-//   console.log(Tone.Transport);
-//   Tone.Transport.scheduleRepeat((time) => {
-//     for (let i = 0; i < limit; i++) {
-//       // create a closure to preserve the value of "i"
-//       str = i.toString(base);
-//       numbers[0].innerHTML = str;
-//       synth.triggerAttackRelease("Bb4", 2, time);
-//     }
-//   });
-//   Tone.Transport.start();
-// }
-
-// console.log(3);
-// count();
+stopButton.addEventListener("click", () => {
+  Tone.Transport.stop();
+});
