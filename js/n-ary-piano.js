de0c23023fc78b744a3f7af4f4c7deea0e378178
Ben Muthalaly <benmuthalaly@gmail.com>
2023-04-05 22:16:01 -0500

    Get Tone.js import working.
    Import is working, and sound is produced after a button click on the
    webpage.

--- js/n-ary-piano.js
+++ js/n-ary-piano.js
@@ -1,23 +1,65 @@
-"use strict";
+// import * as Tone from "tone";
 
+// "use strict";
 // there can be multiple "keyboards"
 // each keyboard has data: a radix, current count
 
 // counts from 0 til base^9
-function count() {
-  let numbers = document.getElementsByClassName("number");
-  let base = 5;
-  let limit = base ** 9;
-  let str = "0";
-  for (let i = 0; i < limit; i++) {
-    create a closure to preserve the value of "i"
-    (function (i) {
-      window.setTimeout(function () {
-    str = i.toString(base);
-    numbers[0].innerHTML = str;
-        }, 1000);
-      })(i);
-  }
-}
+// console.log(1);
+// document.querySelector("button").addEventListener("click", async () => {
+//   await Tone.start();
+// });
 
-count();
+// window.onclick = new AudioContext();
+// let synth = new Tone.PolySynth().toDestination();
+
+addEventListener("click", (event) => {
+  const synth = new Tone.PolySynth().toDestination();
+  // set the attributes across all the voices using 'set'
+  synth.set({ detune: -1200 });
+  // play a chord
+  synth.triggerAttackRelease(["C4", "E4", "A4"], 1);
+});
+
+// console.log(synth.get);
+
+// piano({
+//   parent: document.querySelector("#number"),
+//   polyphonic: true,
+//   noteon: (note) => synth.triggerAttack(note.name),
+//   noteoff: (note) => synth.triggerRelease(note.name),
+// });
+
+// ui({
+//   tone: synth,
+//   parent: document.querySelector("#number"),
+// });
+// console.log(2);
+// function count() {
+//   let numbers = document.getElementsByClassName("number");
+//   let base = 5;
+//   let limit = base ** 9;
+//   let str = "0";
+
+//   const synth = new Tone.PolySynth(8, Tone.Synth, {
+//     oscillator: {
+//       partials: [0, 2, 3, 4],
+//     },
+//   });
+//   // console.log(synth.getOwnPropertyNames());
+//   synth.toDestination();
+//   Tone.Transport.bpm.value = 50;
+//   console.log(Tone.Transport);
+//   Tone.Transport.scheduleRepeat((time) => {
+//     for (let i = 0; i < limit; i++) {
+//       // create a closure to preserve the value of "i"
+//       str = i.toString(base);
+//       numbers[0].innerHTML = str;
+//       synth.triggerAttackRelease("Bb4", 2, time);
+//     }
+//   });
+//   Tone.Transport.start();
+// }
+
+// console.log(3);
+// count();
--- main.go
+++ main.go
@@ -11,6 +11,14 @@ func main() {
 		http.ServeFile(w, r, "./index.html")
 	})
 
+	// http.HandleFunc("/node_modules/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "./node_modules/tone/build/Tone.js")
+	// })
+
+	// http.HandleFunc("/Tone.js.map", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "./node_modules/tone/build/Tone.js.map")
+	// })
+
 	http.HandleFunc("/js/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "js/n-ary-piano.js")
 	})
